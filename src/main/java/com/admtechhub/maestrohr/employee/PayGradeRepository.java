a55f945900f47d4fe92c519e00197efea5869852
Adeyinka <Dotunm85@gmail.com>
2026-05-17 16:30:28 +0100

    download eployees excel

--- src/main/java/com/admtechhub/maestrohr/employee/PayGradeRepository.java
+++ src/main/java/com/admtechhub/maestrohr/employee/PayGradeRepository.java
@@ -1,6 +1,10 @@
 package com.admtechhub.maestrohr.employee;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,6 +12,14 @@ import java.util.UUID;
 
 @Repository
 public interface PayGradeRepository extends JpaRepository<PayGrade, UUID> {
+
     List<PayGrade> findAllByTenantIdAndIsActive(UUID tenantId, boolean isActive);
+
     boolean existsByNameAndTenantId(String name, UUID tenantId);
+
+    @Query("SELECT p FROM PayGrade p WHERE p.tenant.id = :tenantId")
+    List<PayGrade> findAllByTenantId(@Param("tenantId") UUID tenantId);
+
+    @Query("SELECT p FROM PayGrade p WHERE p.tenant.id = :tenantId")
+    Page<PayGrade> findAllByTenantId(@Param("tenantId") UUID tenantId, Pageable pageable);
 }
